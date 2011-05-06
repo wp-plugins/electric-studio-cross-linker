5a8e7609fb17038d29adb7e1b211b582f9d84e91
irvingswiftj <irvingswiftj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-06 09:09:41 +0000

    Fixing version 1.0 trunk
    git-svn-id: https://plugins.svn.wordpress.org/electric-studio-cross-linker/trunk@381636 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- electric-studio-cross-linker.php
+++ electric-studio-cross-linker.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: Electric Studio Cross Linker
-Plugin URI: http://www.electricstudio.co.uk
+Plugin URI: http://www.electricstudio.co.uk/2010/09/electric-studio-cross-linking-plugin-for-wordpress/
 Description: A plugin to make references to posts in your site's content into cross-links.
-Version: 0.9.5
+Version: 1.0
 Author: James Irving-Swift
 Author URI: http://www.irving-swift.com
 License: GPL2
@@ -15,22 +15,23 @@ include 'php/options.php';
 add_action('template_redirect', 'add_my_script');
  
 function add_my_script() {
-	wp_enqueue_script('highlighter', WP_CONTENT_URL . '/plugins/ES_tag_highlighter/js/highlighter.js', array('jquery'), '1.0', true);
+	wp_enqueue_script('highlighter', WP_CONTENT_URL . '/plugins/electric-studio-cross-linker/js/highlighter.js', array('jquery'), '1.0', true);
 }
 
 
 /* This calls ES_tag_highlighter() function when wordpress adds scripts.*/
-add_action('wp_print_footer_scripts','ES_tag_highlighter');
-function ES_tag_highlighter(){
+add_action('wp_print_footer_scripts','electric_studio_cross_linker');
 
-global $wpdb;
-global $post;
-$excluded_pages = get_option('selectpage');
-$pluralForms = get_option('PluralForms');
-$exactMatchesOnly = get_option('exactMatchesOnly');
+function electric_studio_cross_linker(){
+
+  global $wpdb;
+  global $post;
+  $excluded_pages = get_option('selectpage');
+  $pluralForms = get_option('PluralForms');
+  $exactMatchesOnly = get_option('exactMatchesOnly');
 
   //sql string
-  $sql = "SELECT ID, post_title FROM wp_posts";
+  $sql = "SELECT ID, post_title FROM $wpdb->posts";
   //execute sql command  
   $page_titles = $wpdb->get_results($sql);
 
@@ -44,8 +45,20 @@ $exactMatchesOnly = get_option('exactMatchesOnly');
     $excluded_tags = get_option('selectpost');
   }
 
+  $pagesToExclude = get_option('selectpage');
+
+
+  //the follow two IF statement, makes these variables into blank arrays if they are empty (this stop PHP warnings when using in_array())
+  if(!is_array($pagesToExclude)){
+    $pagesToExclude = array();
+  }
+
+  if(!is_array($excluded_pages)){
+    $excluded_pages = array();
+  }
+
   //that that the array is not empty in the databse
-  if(get_option('selectpage')!=NULL){
+  if(!in_array("post".$post->ID,$pagesToExclude)){
     //check that this page is not an excluded page
     if(!in_array("post".$post->ID,$excluded_pages)){
 
@@ -61,22 +74,17 @@ $exactMatchesOnly = get_option('exactMatchesOnly');
         echo 'var pluralForms="";';
       }
 
-	//This is a redundant feature
-      //if statement to set the exactMatchesOnly into the javascript
-      //if($exactMatchesOnly=="true"){
-        //echo 'var exactMatchesOnly=" ";';
-      //}else{
-      //  echo 'var exactMatchesOnly="";';
-      //}
-    echo 'var exactMatchesOnly=" ";';
+    echo '
+    ';
       
       
       $i = 0;
       foreach($page_titles as $page_title){
         if(!in_array("post".$page_title->ID,$excluded_tags)){  
         echo 'titles['.$i.'] = new Array(2);
-              titles['.$i.'][0] = "'.$page_title->post_title.'";
-              titles['.$i.'][1] = "'.$page_title->ID.'";';
+                titles['.$i.'][0] = "'.$page_title->post_title.'";
+                titles['.$i.'][1] = "'.$page_title->ID.'";
+              ';
         }
         $i++;
       }
